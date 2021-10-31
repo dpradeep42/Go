3d306135bf3a998f21413ef21f4251c8409b835f
Pradeep Kumar D <dpradeep42@gmail.com>
2021-10-31 20:47:53 +0530

    Added Variables

---
+++ Variables.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	//A Constant is a variable with a value that can't be changed
+
+	const pi float64 = 3.14159265359
+
+	//You can declare multiple variables like this
+
+	var (
+		varA = 2
+		varB = 3
+	)
+
+	fmt.Println(varA, varB)
+
+	//Strings are a series of characters between " or '
+
+	var Name string = "Pradeep Kumar"
+
+	//Get string length
+
+	fmt.Println(len(Name))
+}
