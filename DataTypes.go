6d959a5118f8133e7711b68821fb9b7866809212
Pradeep Kumar D <dpradeep42@gmail.com>
2021-11-02 08:35:16 +0530

    Added Arithmetic Operators and Pointers and modified Data Types

---
+++ DataTypes.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a int = 5
+	var b float32 = 4.32
+	const pi float64 = 3.1415139475
+
+	x, y := 14, 15
+
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(pi)
+	fmt.Println(x)
+	fmt.Println(y)
+}
